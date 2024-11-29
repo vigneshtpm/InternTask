f8c00b6a91b0eec2765bdef46960111c6f17c234
vigneshtpm <vigneshwarantpm2@gmail.com>
2024-11-29 20:40:59 +0530

    3 Commit

--- lib/services/editprofile.dart
+++ lib/services/editprofile.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:interntask/services/utils.dart';
+import 'package:interntask/utils/utils.dart';
 import 'package:interntask/state/profile_controller.dart'; // Import your controller
 
 class EditProfilePage extends StatelessWidget {
